a0b90c59c49f11fcd1682f2eacfb24876268bba1
Ryobia <ryobia36@gmail.com>
2021-09-10 15:29:30 -0600

    wow that was hard but I made it so the arrow icon will always point at themouse cursor. now to make it shoot stuff.

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
       <section id="main">
         <Game />
       </section>
+      <div className="bot">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     </div>
   );
 }
--- client/src/components/Game/index.js
+++ client/src/components/Game/index.js
@@ -1,15 +1,36 @@
 import React, { useState } from "react";
+import ReactDOM from 'react-dom'
+import arrow from '../../images/next.png';
+
 
 function Game() {
 
+  const [mouseX, setMouseX] = useState(0);
+  const [mouseY, setMouseY] = useState(0);
+
+
     window.addEventListener("click", function() {
         console.log('click');
+
+    });
+
+    window.addEventListener("mousemove", function(e) {
+      setMouseX(e.x);
+      setMouseY(e.y);
+      let player = document.getElementById('arrow');
+
+      let playerX = (player.getBoundingClientRect().left) + (player.clientWidth / 2);
+      let playerY = (player.getBoundingClientRect().top) + (player.clientHeight / 2);
+      let radian = Math.atan2(e.x - playerX, e.y - playerY);
+      let rot = (radian * (180 / Math.PI) * -1) + 88;
+      player.style.transform = "rotate("+ rot + "deg)";
+
     });
 
     return (
-      <div id="board">
-        <div className="player">
-        0
+      <div id="canvas">
+        <div id="arrow" className="player">
+        <img src={arrow} height="30px"/>
         </div>
         <div className="enemy">
         
