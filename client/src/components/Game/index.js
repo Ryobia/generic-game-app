a746498d863748ccc235545a881eeff4206a54f9
Ryobia <ryobia36@gmail.com>
2021-09-11 12:50:06 -0600

    eh, bullet spawns and the things spins properly

--- client/src/components/Game/index.js
+++ client/src/components/Game/index.js
@@ -1,36 +1,52 @@
-import React, { useState } from "react";
-import ReactDOM from 'react-dom'
-import arrow from '../../images/next.png';
+import React, { useState, createRef, useRef } from "react";
+import arrow from '../../images/arrow.png';
 
 
 function Game() {
+  
 
   const [mouseX, setMouseX] = useState(0);
   const [mouseY, setMouseY] = useState(0);
 
 
-    window.addEventListener("click", function() {
-        console.log('click');
 
-    });
 
     window.addEventListener("mousemove", function(e) {
       setMouseX(e.x);
       setMouseY(e.y);
+
+    });
+
+   let field = function()  {
       let player = document.getElementById('arrow');
 
       let playerX = (player.getBoundingClientRect().left) + (player.clientWidth / 2);
       let playerY = (player.getBoundingClientRect().top) + (player.clientHeight / 2);
-      let radian = Math.atan2(e.x - playerX, e.y - playerY);
-      let rot = (radian * (180 / Math.PI) * -1) + 88;
+      let radian = Math.atan2(mouseX - playerX, mouseY - playerY);
+      let rot = (radian * (180 / Math.PI) * -1) + 90;
       player.style.transform = "rotate("+ rot + "deg)";
+    }
 
-    });
+   let fire = function() {
+     let canvas = document.getElementById('canvas');
+     let bullet = document.createElement('p');
+     bullet.innerHTML = '⚫';
+     bullet.classList.add('bullet');
+     canvas.appendChild(bullet);
+     let left = bullet.getBoundingClientRect().left;
+     let top = bullet.getBoundingClientRect().top;
+     let newLeft = mouseX - left;
+     let newTop = mouseY - top;
+     console.log(`mL:${mouseX}, mT:${mouseY}, L:${left}, T:${top}, NL:${newLeft}, NT:${newTop}`);
+     bullet.style.left = `${newLeft}px`;
+     bullet.style.top = `${newTop}px`;
+   } 
+  
 
     return (
-      <div id="canvas">
+      <div id="canvas" className="canvas" onMouseMove={field} onClick={fire}>
         <div id="arrow" className="player">
-        <img src={arrow} height="30px"/>
+        <img id="playerIcon" src={arrow} height="30px" width="30px"/>
         </div>
         <div className="enemy">
         
